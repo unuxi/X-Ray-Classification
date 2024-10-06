46862b8c9119768df052e4f695bb918e8fe8c5cb
unuxi <147153033+unuxi@users.noreply.github.com>
2024-10-06 19:46:20 +0200

    Upload project files

---
+++ config.py
@@ -0,0 +1,17 @@
+'''
+@author: Robin Schumacher
+
+In dieser Datei werden die nötigen Configuarationsdaten für die Modelle festgelegt.
+Für die config von alten Modellen siehe Ordner output.
+
+'''
+
+train_path = 'C:/Users/robsc/Documents/Studium/Semester 6/Data Science/Portfolio/Datenset/neu/data/train'
+val_path = 'C:/Users/robsc/Documents/Studium/Semester 6/Data Science/Portfolio/Datenset/neu/data/validation'
+test_path = 'C:/Users/robsc/Documents/Studium/Semester 6/Data Science/Portfolio/Datenset/neu/data/test'
+
+IMG_SIZE = [300, 300]
+BATCH_SIZE = 10
+NUM_CLASSES = 3
+
+EPOCHS = 10
